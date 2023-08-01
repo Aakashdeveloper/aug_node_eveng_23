4f738506a5c1f3835939d2e1ec9b406700107809
aakashdeveloper <ahanda205@gmail.com>
2023-08-01 20:52:04 +0530

    weatherapp

--- fullstack/src/controller/productRouter.js
+++ fullstack/src/controller/productRouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let productRouter = express.Router();
 let {getData} = require('./dbcontroller');
+let mongo = require('mongodb')
 
 function router(menu){
     
@@ -20,9 +21,13 @@ function router(menu){
         res.render('products',{title:'Products Page',products,menu})
     })
 
-    productRouter.route('/details')
-        .get(function(req,res){
-        res.send('Products Details')
+    productRouter.route('/details/:id')
+        .get(async function(req,res){
+            //let id = req.params.id;
+            let {id} = req.params;
+            let query = {_id:mongo.ObjectId(id)}
+            let products = await getData('products',query)
+            res.render('products',{title:'Products Page',products,menu})
     })
 
     return productRouter
