221828bd90858d3ef8cf5d457de1ddb4ec25b84f
aakashdeveloper <ahanda205@gmail.com>
2023-08-04 20:18:47 +0530

    redis4

--- redis4/app.js
+++ redis4/app.js
@@ -9,8 +9,26 @@ let client = createClient({
     port:6379
 })
 
-app.get('/data',(req,res) => {})
+client.on('error',err=> console.log(`Redis Client Error`,err))
 
+app.get('/data',async(req,res) => {
+    await client.connect();
+    let userInput = req.query.country.trim();
+    userInput = userInput?userInput:'India';
+    const url = `https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=${userInput}`;
+    let result = await client.get(userInput)
+    if(result){
+        let ouput = JSON.parse(result);
+        res.send(ouput)
+    }else{
+        let apiResponse = await axios.get(url);
+        let output = apiResponse.data;
+        await client.set(userInput,JSON.stringify({source:'Redis Cache',output}),{EX:10,NX:true})
+        res.send({source:'Api Response',output})
+    }
+
+    await client.disconnect();
+})
 
 
 app.listen(port,(err) => {
