99321cbcf389bfff76f7529f5d2420b0d6cec576
aakashdeveloper <ahanda205@gmail.com>
2023-08-31 20:54:57 +0530

    nodemailer

--- nodemailer/app.js
+++ nodemailer/app.js
@@ -2,3 +2,24 @@ let nodemailer = require('nodemailer');
 let dotenv = require('dotenv');
 dotenv.config();
 
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'ahanda205@gmail.com',
+        pass:process.env.PASS
+    }
+})
+
+let mailOptions = {
+    form:'ahanda205@gmail.com',
+    to:'ahanda206@hotmail.com',
+    subject:'Sending Email Aug Node',
+    text:'This is text and test email'
+}
+
+transporter.sendMail(mailOptions,(err,info) => {
+    if(err) console.log(err);
+    else{
+        console.log(`Email sent: ${info.response}`)
+    }
+})
